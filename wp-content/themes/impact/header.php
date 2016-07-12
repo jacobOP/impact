5bda5d4a9b34c6aef92f140388aea7cf22749b25
Jacob Harris <jacob@outerplaces.com>
2016-07-12 17:11:46 -0400

    add all favicons, update asskicker banner padding

--- wp-content/themes/impact/header.php
+++ wp-content/themes/impact/header.php
@@ -28,7 +28,13 @@ ACTION ON DEMAND
 
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
+
+<link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_stylesheet_directory_uri(); ?>/favicons/apple-touch-icon.png">
+<link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/favicons/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/favicons/favicon-16x16.png" sizes="16x16">
+<link rel="manifest" href="<?php echo get_stylesheet_directory_uri(); ?>/favicons/manifest.json">
+<link rel="mask-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicons/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="theme-color" content="#f89a1c">
 
 <?php // We are loading our theme directory style.css by queuing scripts in our functions.php file, 
 	// so if you want to load other stylesheets,
--- wp-content/themes/impact/index.php
+++ wp-content/themes/impact/index.php
@@ -458,30 +458,5 @@ get_header(); // This fxn gets the header.php file and renders it ?>
 			</div> <!-- .container -->
 		</section> <!-- .asskicker -->
 	</div><!-- #primary  -->
-	<script>
-		jQuery(document).ready(function(){
-		    var ajaxUrl = "<?php echo admin_url('admin-ajax.php')?>";
-		    var page = 1; // What page we are on.
-		    var ppp = 3; // Post per page
-
-		    jQuery("#more_posts").on("click",function(){ // When btn is pressed.
-		        jQuery("#more_posts").attr("disabled",true); // Disable the button, temp.
-		        jQuery.post(ajaxUrl, {
-		            action:"more_post_ajax",
-		            offset: (page * ppp) + 1,
-		            ppp: ppp
-		        }).success(function(posts){
-		        	if (!posts){
-		        		console.log('false');
-		        		jQuery('.js_more-posts-container').html('<p style="text-align:center">No more articles to display</p>');
-		        	} else {
-		        		 page++;
-			            jQuery(".js_insert_more_posts").append(posts); // CHANGE THIS!
-			            jQuery("#more_posts").attr("disabled",false);
-		        	}
-		           
-		        });
-		   });
-		});
-	</script>
+	<?php include 'ajax-footer.php' ?>
 <?php get_footer(); // This fxn gets the footer.php file and renders it ?>
